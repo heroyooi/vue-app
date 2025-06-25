cc34b93c062b9b6a6392e6305ae2da6512d64da1
heroyooi <heroyooi1018@gmail.com>
2025-06-26 08:44:47 +0900

    Revert "2강: 반응형 데이터 다뤄보기 (ref vs reactive)"
    This reverts commit 5b0129d971680a0e0f3483a181fe75472b46d682.

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,18 @@
 <template>
   <div>
-    <h2>이름을 입력해보세요</h2>
-    <input v-model="name" placeholder="이름 입력" />
-    <p>안녕하세요, {{ name }}님!</p>
-
-    <h2>reactive 객체 실습</h2>
-    <input v-model="user.firstName" placeholder="이름 입력" />
-    <input v-model="user.lastName" placeholder="성 입력" />
-    <p>안녕하세요, {{ user.firstName }} {{ user.lastName }}님!</p>
+    <h1>{{ message }}</h1>
+    <p>Vue 3 튜토리얼에 오신 것을 환영합니다!</p>
   </div>
 </template>
 
 <script setup>
-import { ref, reactive } from 'vue'
+import { ref } from 'vue'
 
-const name = ref('홍길동') // 단일 값
-const user = reactive({
-  firstName: '홍',
-  lastName: '길동'
-}) // 객체
+const message = ref('안녕하세요, Vue 3!')
 </script>
 
 <style scoped>
-input {
-  margin: 4px;
-  padding: 4px;
+h1 {
+  color: #42b983;
 }
 </style>
